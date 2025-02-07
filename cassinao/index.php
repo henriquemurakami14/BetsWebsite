9be461c67cfa27ece9cc7746fb287e3d399ebf21
gabriel0als <gabrielaugusto.ifpr2024@gmail.com>
2025-02-06 21:37:08 -0300

    index.php

--- cassinao/index.php
+++ cassinao/index.php
@@ -17,19 +17,19 @@
         <h1 class="titulo">Selecione um dos jogos abaixo para gerar seus números da sorte!</h1>
         <p class="texto">Escolha qual jogo deseja jogar: </p>
 
-        <p><a href="http://localhost/aulaphp/cassinao/mega.php"><span class="links">MEGA-SENA</span></a></p>  
-        <p><a href="http://localhost/aulaphp/cassinao/quina.php"><span class="links">QUINA</span></a></p>
-        <p><a href="http://localhost/aulaphp/cassinao/lotomania.php"><span class="links">LOTOMANIA</span></a></p>
-        <p><a href="http://localhost/aulaphp/cassinao/lotofacil.php"><span class="links">LOTOFACIL</span></a></p>
+        <p><a class="botao" href="http://localhost/cassinao/mega.php">MEGA-SENA</a></p>  
+        <p><a class="botao" href="http://localhost/cassinao/quina.php">QUINA</a></p>
+        <p><a class="botao" href="http://localhost/cassinao/lotomania.php">LOTOMANIA</a></p>
+        <p><a class="botao" href="http://localhost/cassinao/lotofacil.php">LOTOFACIL</a></p>
         </div>
         
         <br>
         <br>
-        <div class="caixa-imagens">
-            <img src="mega sena.jpg" alt="mega sena" width="290" height="170">
-            <img src="quina.png" alt="quina" width="290" height="170">
-            <img src="lotomania.png" alt="lotomania" width="290" height="170">
-            <img src="lotofacil.png" alt="lotofacil" width="290" height="170">
+        <div class="caixa-maior">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJkhyeE68t32B4j3l-v2yT05uL2HjmPVD3lQ&s" alt="mega sena logo" width="290" height="170">
+            <img src="https://portalcantu.com.br/news/images/2024/12/25/(02)-PortalCantu-25-12-2024.png" alt="quina logo" width="290" height="170">
+            <img src="https://uploads.spacemoney.com.br/2024/07/8UKLT9MF-Lotomania-logo-1280x720-1.webp" alt="lotomania logo" width="290" height="170">
+            <img src="https://sinlopar.com.br/wp-content/uploads/2023/12/lotofacil-1.png" alt="lotofacil logo" width="290" height="170">
 
             </div>
     </main>
