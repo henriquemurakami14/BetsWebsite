fc3209b6553ee441bfc434278d1894c3dbaebd0c
gabriel0als <gabrielaugusto.ifpr2024@gmail.com>
2025-02-06 21:37:40 -0300

    lotofacil.php

--- cassinao/lotofacil.php
+++ cassinao/lotofacil.php
@@ -4,30 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LotoFacil-365</title>
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
     <header>
-        <h1>Bem Vindos a LOTOFACIL-365</h1><hr>
+    <div class="caixa-centralizada">
+        <h1 class="titulo-principal">Bem Vindos a <span class="palavra-chave">LOTOFACIL-365</span></h1>
+        </div>
+        <hr>
+        <br>
+        <div class="caixa-centralizada">
+            <img src="https://sinlopar.com.br/wp-content/uploads/2023/12/lotofacil-1.png" alt="lotofacil logo" width=340 height=220>
+
         
         <?php
             sleep(1);
             if (isset($_REQUEST['mensagem'])) {
-                echo "<p>{$_REQUEST['mensagem']}</p>";
+                echo "<p class='erro'>{$_REQUEST['mensagem']}</p>";
             }
         ?>
 
         <section>
             <form action="cad.php" method="get">
+                <br>
                 <input type="hidden" name="tipo_jogo" value="LOTOFACIL">
-                <label for="idJogo">Quantidade de números da aposta:</label>
+                <label for="idJogo" class="texto">Quantidade de números da aposta:</label>
                 <input type="num" name="numeros_jogados" id="idJogo">
-                <label for="rangeInput">Quantos jogos você deseja fazer:</label>
-                <input type="range" id="rangeInput" min="1" max="10" step="1" value="1" oninput="output.value = rangeInput.value">
+                <br>
+                <label for="rangeInput" class="texto">Quantos jogos você deseja fazer:</label>
+                <input type="range" id="rangeInput" name="quantidade_jogos" min="1" max="10" step="1" value="1" oninput="output.value = rangeInput.value">
                 <output id="output">1</output>
-                <input type="submit" value="Gerar">
-                
+                <br><br>
+                <input type="submit" class="gerar" value="Gerar">
             </form>
+            </div>
         </section>
     </header>
 </body>
