c34899bd697a1e6d9a20749705b9fc673b216fe8
gabriel0als <gabrielaugusto.ifpr2024@gmail.com>
2025-02-06 21:38:56 -0300

    quina.php

--- cassinao/quina.php
+++ cassinao/quina.php
@@ -4,30 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mega-On</title>
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
     <header>
-        <h1>Bem Vindos a QUINA-JOGOS</h1><hr>
+    <div class="caixa-centralizada">
+        <h1 class="titulo-principal">Bem Vindos a <span class="palavra-chave">QUINA JOGOS</span></h1>
+        </div>
+        <hr>
+        <br>
+        <div class="caixa-centralizada">
+            <img src="https://portalcantu.com.br/news/images/2024/12/25/(02)-PortalCantu-25-12-2024.png" alt="mega sena logo" width=340 height=220>
         
         <?php
             sleep(1);
             if (isset($_REQUEST['mensagem'])) {
-                echo "<p>{$_REQUEST['mensagem']}</p>";
+                echo "<p class='erro'>{$_REQUEST['mensagem']}</p>";
             }
         ?>
-
+        
         <section>
-            <form action="cad.php" method="get">
+                <br>
+                <form action="cad.php" method="get">
                 <input type="hidden" name="tipo_jogo" value="QUINA">
-                <label for="idJogo">Quantidade de números da aposta:</label>
+                <label for="idJogo" class="texto">Quantidade de números da aposta:</label>
                 <input type="num" name="numeros_jogados" id="idJogo">
-                <label for="rangeInput">Quantos jogos você deseja fazer:</label>
-                <input type="range" id="rangeInput" min="1" max="10" step="1" value="1" oninput="output.value = rangeInput.value">
+                <br>
+                <label for="rangeInput" class="texto">Quantos jogos você deseja fazer:</label>
+                <input type="range" name="quantidade_jogos" id="rangeInput" min="1" max="10" step="1" value="1" oninput="output.value = rangeInput.value">
                 <output id="output">1</output>
-                <input type="submit" value="Gerar">
-                
+                <br><br>
+                <input type="submit" class="gerar" value="Gerar">
             </form>
+            </div>
         </section>
     </header>
 </body>
